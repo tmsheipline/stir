2a43e78410d417be01a66e9749d27f5b46226b5a
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-06-16 14:13:45 -0400

    changed line 33 server.js mongodbURI

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(methodOverride("_method")); // override for put and delete requests from
 app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
 app.use(express.static("public")); // serve files from public statically
 
-app.use(session({secret: process.env.SECRET, store: MongoStore.create({ mongoUrl: process.env.DATABASE_URL}),
+app.use(session({secret: process.env.SECRET, store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI}),
 saveUninitialized: true,
 resave: false,
 }))
